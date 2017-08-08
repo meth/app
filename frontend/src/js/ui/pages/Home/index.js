ab4fce5eabccdf56b2162d5c7845cb3f3d8724ca
Ramesh Nair <ram@hiddentao.com>
2017-08-08 21:20:22 +0100

    testing overlays

--- frontend/src/js/ui/pages/Home/index.js
+++ frontend/src/js/ui/pages/Home/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 
 // import dispatcher from '../../../data/dispatcher'
+import styles from './styles'
 
 export default class Page extends Component {
   render () {
+    const OverlayDiv = styles.overlayDiv()
     return (
       <div>
         <p>test</p>
+        <OverlayDiv />
         <webview src="https://github.com" style={{
           display: 'inline-flex',
           width: '640px',
---
+++ frontend/src/js/ui/pages/Home/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export default {
+  overlayDiv: () => styled.div`
+    position: absolute;
+    top: 100px;
+    left: 100px;
+    height: 50px;
+    padding: 50px;
+    background-color: #f00;
+  `,
+}
