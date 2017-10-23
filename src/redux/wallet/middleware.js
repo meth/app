8b9943acc4c3735d4080f46451d06d4330b76af0
Ramesh Nair <ram@hiddentao.com>
2017-10-23 01:56:28 +0100

    hook up raw tx to actual call

--- src/redux/wallet/middleware.js
+++ src/redux/wallet/middleware.js
@@ -2,13 +2,16 @@ import { SEND_RAW_TX, GENERATE_RAW_TX } from './actions'
 import { createAction } from '../utils'
 
 // eslint-disable-next-line consistent-return
-export default () => () => next => async action => {
+export default ({ nodeConnector }) => () => next => async action => {
   switch (action.type) {
     case GENERATE_RAW_TX: {
       return Promise.resolve('0xdeadbeef')
     }
     case SEND_RAW_TX: {
-      const receipt = 123
+      const rawTx = action.payload
+
+      const receipt =
+        await nodeConnector.rawCall('eth_sendRawTransaction', [ rawTx ])
 
       await next(createAction(action.type, receipt))
 
