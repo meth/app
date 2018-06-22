bcdd0c2a2ee708fbef37542f152296fcb5014663
Ramesh Nair <ram@hiddentao.com>
2018-06-22 10:13:52 +0100

    better random string generator

--- src/utils/string.js
+++ src/utils/string.js
@@ -4,4 +4,11 @@ export const prefixWith0x = str => `0x${str}`
 
 export const prefixedWith0x = str => str.substr(0, 2) === '0x'
 
-export const randStr = len => Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, len)
+const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+export const randStr = len => {
+  let str = ''
+  for (let i = 0; len > i; i += 1) {
+    str += CHARS.charAt(Math.floor(Math.random() * CHARS.length))
+  }
+  return str
+}
