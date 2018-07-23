9d52e0e79433e396b16f89de3dcda1d135ea5f5d
Ramesh Nair <ram@hiddentao.com>
2018-07-23 12:53:24 +0800

    number tests done

--- src/utils/number.test.js
+++ src/utils/number.test.js
@@ -9,7 +9,15 @@ import {
   toFloat,
   toFloatStr,
   toInt,
-  toIntStr
+  toIntStr,
+  toTokenRawBalanceBigNum,
+  toTokenFriendlyBalanceBigNum,
+  weiToEthBigNum,
+  ethToWeiBigNum,
+  gweiToWeiBigNum,
+  weiToGweiBigNum,
+  calculateGasCostInWeiBigNum,
+  getTotalAccountsBalanceAsStr
 } from './number'
 
 describe('.toBigNum()', () => {
@@ -17,7 +25,9 @@ describe('.toBigNum()', () => {
     expect(toBigNum(2.3)).toBeInstanceOf(BigNumber)
     expect(toBigNum(2.3).toString()).toEqual('2.3')
     expect(toBigNum(2).toString()).toEqual('2')
-    expect(toBigNum(Number.MAX_SAFE_INTEGER).toString()).toEqual('9007199254740991')
+    expect(toBigNum(Number.MAX_SAFE_INTEGER).toString()).toEqual(
+      '9007199254740991'
+    )
     expect(toBigNum('0xdeadbeef', 16).toString()).toEqual('3735928559')
     expect(toBigNum('deadbeef', 16).toString()).toEqual('3735928559')
     expect(toBigNum('zxw').toString()).toEqual('NaN')
@@ -31,11 +41,14 @@ describe('.hexStrToNumber()', () => {
   })
 })
 
-
 describe('.hexStrToBigNum()', () => {
   it('works as expected', () => {
-    expect(hexStrToBigNum('0xdeadbeef').toNumber()).toEqual(hexStrToNumber('0xdeadbeef'))
-    expect(hexStrToBigNum('deadbeef').toNumber()).toEqual(hexStrToNumber('deadbeef'))
+    expect(hexStrToBigNum('0xdeadbeef').toNumber()).toEqual(
+      hexStrToNumber('0xdeadbeef')
+    )
+    expect(hexStrToBigNum('deadbeef').toNumber()).toEqual(
+      hexStrToNumber('deadbeef')
+    )
   })
 })
 
@@ -60,8 +73,12 @@ describe('.toFormattedDecStr()', () => {
   })
 
   it('can avoid commas', () => {
-    expect(toFormattedDecStr('255.2744', 2, { showCommas: false })).toEqual('255.27')
-    expect(toFormattedDecStr('2558761.2754', 2, { showCommas: false })).toEqual('2558761.28')
+    expect(toFormattedDecStr('255.2744', 2, { showCommas: false })).toEqual(
+      '255.27'
+    )
+    expect(toFormattedDecStr('2558761.2754', 2, { showCommas: false })).toEqual(
+      '2558761.28'
+    )
   })
 
   it('trims excess 0s', () => {
@@ -100,5 +117,82 @@ describe('.toIntStr()', () => {
     expect(toIntStr('abc')).toEqual('')
     expect(toIntStr('1234.234')).toEqual('1234')
     expect(toIntStr('-1234')).toEqual('-1234')
+  })
+})
+
+describe('.toTokenRawBalanceBigNum', () => {
+  it('works as expected', () => {
+    const result = toTokenRawBalanceBigNum('255.23432239', 18)
+
+    expect(result).toBeInstanceOf(BigNumber)
+    expect(result.toString()).toEqual('255234322390000000000')
+  })
+})
+
+describe('.toTokenFriendlyBalanceBigNum', () => {
+  it('works as expected', () => {
+    const result = toTokenFriendlyBalanceBigNum('255234322390000000000', 18)
+
+    expect(result).toBeInstanceOf(BigNumber)
+    expect(result.toString()).toEqual('255.23432239')
+  })
+})
+
+describe('.weiToEthBigNum', () => {
+  it('works as expected', () => {
+    const result = weiToEthBigNum('255234322390000000000')
+
+    expect(result).toBeInstanceOf(BigNumber)
+    expect(result.toString()).toEqual('255.23432239')
+  })
+})
+
+describe('.ethToWeiBigNum', () => {
+  it('works as expected', () => {
+    const result = ethToWeiBigNum('255.23432239')
+
+    expect(result).toBeInstanceOf(BigNumber)
+    expect(result.toString()).toEqual('255234322390000000000')
+  })
+})
+
+describe('.gweiToWeiBigNum', () => {
+  it('works as expected', () => {
+    const result = gweiToWeiBigNum('25523432239')
+
+    expect(result).toBeInstanceOf(BigNumber)
+    expect(result.toString()).toEqual('25523432239000000000')
+  })
+})
+
+describe('.weiToGweiBigNum', () => {
+  it('works as expected', () => {
+    const result = weiToGweiBigNum('25523432239000000000')
+
+    expect(result).toBeInstanceOf(BigNumber)
+    expect(result.toString()).toEqual('25523432239')
+  })
+})
+
+describe('.calculateGasCostInWeiBigNum', () => {
+  it('works as expected', () => {
+    const result = calculateGasCostInWeiBigNum(21000, 23)
+
+    expect(result).toBeInstanceOf(BigNumber)
+    expect(result.toString()).toEqual('483000000000000')
+  })
+})
+
+describe('.getTotalAccountsBalanceAsStr', () => {
+  it('works as expected', () => {
+    const accounts = {
+      1: { balance: ethToWeiBigNum(23000.294) },
+      2: { balance: ethToWeiBigNum(87) },
+      3: { balance: ethToWeiBigNum(1098.9) }
+    }
+
+    const result = getTotalAccountsBalanceAsStr(accounts)
+
+    expect(result).toEqual('Ξ 24,186.2')
   })
 })
